87d2504823249a26a935116b9669225d18d758a3
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-05-08 10:57:59 +0530

    Configure MySQL database connection

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+
+    try {
+
+        const connection = await db.getConnection();
+
+        res.send('Database Connected Successfully');
+
+        connection.release();
+
+    } catch (error) {
+
+        res.status(500).send(error.message);
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
