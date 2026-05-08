6b3ae1dab11a1103d0e6d5ee1c16af18cbe63552
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-05-08 15:08:33 +0530

    Add: quantity measurement database queries

--- models/quantityModel.js
+++ models/quantityModel.js
@@ -1,29 +1,46 @@
 const db = require('../config/db');
 
 async function saveMeasurement(data) {
-
     const sql = `
         INSERT INTO quantity_measurements (
-            operation_type,
-            unit_type,
-            value1,
-            unit1,
-            value2,
-            unit2,
+            operation,
+
+            this_value,
+            this_unit,
+            this_measurement_type,
+
+            that_value,
+            that_unit,
+            that_measurement_type,
+
             result_value,
-            result_unit
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            result_unit,
+            result_measurement_type,
+            result_text,
+
+            is_error,
+            error_message
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     const values = [
-        data.operation_type,
-        data.unit_type,
-        data.value1,
-        data.unit1,
-        data.value2,
-        data.unit2,
+        data.operation,
+
+        data.this_value,
+        data.this_unit,
+        data.this_measurement_type,
+
+        data.that_value,
+        data.that_unit,
+        data.that_measurement_type,
+
         data.result_value,
-        data.result_unit
+        data.result_unit,
+        data.result_measurement_type,
+        data.result_text,
+
+        data.is_error || false,
+        data.error_message || null
     ];
 
     const [result] = await db.execute(sql, values);
@@ -32,7 +49,6 @@ async function saveMeasurement(data) {
 }
 
 async function getAllMeasurements() {
-
     const [rows] = await db.execute(
         'SELECT * FROM quantity_measurements ORDER BY created_at DESC'
     );
@@ -40,7 +56,46 @@ async function getAllMeasurements() {
     return rows;
 }
 
+async function getOperationHistory(operation) {
+    const [rows] = await db.execute(
+        'SELECT * FROM quantity_measurements WHERE operation = ? ORDER BY created_at DESC',
+        [operation]
+    );
+
+    return rows;
+}
+
+async function getMeasurementsByType(type) {
+    const [rows] = await db.execute(
+        'SELECT * FROM quantity_measurements WHERE this_measurement_type = ? ORDER BY created_at DESC',
+        [type]
+    );
+
+    return rows;
+}
+
+async function getOperationCount(operation) {
+    const [rows] = await db.execute(
+        'SELECT COUNT(*) AS count FROM quantity_measurements WHERE operation = ? AND is_error = false',
+        [operation]
+    );
+
+    return rows[0].count;
+}
+
+async function getErrorHistory() {
+    const [rows] = await db.execute(
+        'SELECT * FROM quantity_measurements WHERE is_error = true ORDER BY created_at DESC'
+    );
+
+    return rows;
+}
+
 module.exports = {
     saveMeasurement,
-    getAllMeasurements
+    getAllMeasurements,
+    getOperationHistory,
+    getMeasurementsByType,
+    getOperationCount,
+    getErrorHistory
 };
