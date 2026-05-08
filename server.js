a95db893f6fb06785431d122e422112b0d09166a
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-05-08 15:59:47 +0530

    Update: server route configuration

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/quantity', quantityRoutes);
+app.use('/api/v1/quantities', quantityRoutes);
 
 app.get('/', (req, res) => {
     res.send('Quantity Measurement API Running');
