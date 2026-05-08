8b80f2f2711f9538c113c9cdd75c0b62f998d34c
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-05-08 11:01:57 +0530

    Add measurement unit definitions

---
+++ models/units.js
@@ -0,0 +1,25 @@
+const LENGTH_UNITS = {
+    meter: 1,
+    centimeter: 0.01,
+    kilometer: 1000,
+    foot: 0.3048,
+    inch: 0.0254
+};
+
+const WEIGHT_UNITS = {
+    gram: 1,
+    kilogram: 1000,
+    pound: 453.592
+};
+
+const VOLUME_UNITS = {
+    liter: 1,
+    milliliter: 0.001,
+    gallon: 3.78541
+};
+
+module.exports = {
+    LENGTH_UNITS,
+    WEIGHT_UNITS,
+    VOLUME_UNITS
+};
